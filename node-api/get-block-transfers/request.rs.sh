cc39e5324f9b2078b2d213beb308a91ba3fe6098
Mark A. Greenslade <mark@casperlabs.io>
2021-05-03 17:00:10 +0200

    1. Added example rust client usage.

--- node-api/get-block-transfers/request.rs.sh
+++ node-api/get-block-transfers/request.rs.sh
@@ -13,27 +13,27 @@ _BLOCK_HASH="c7148e1e2e115d8fba357e04be2073d721847c982dc70d5c36b5f6d3cf66331c"
 # A known block height.
 _BLOCK_HEIGHT=20652
 
-# Set latest block.
+# Set latest transfers.
 _BLOCK_LATEST=$(
-    $CASPER_CLIENT get-block \
+    $CASPER_CLIENT get-block-transfers \
         --node-address "$_NODE_ADDRESS" \
-        | jq '.result.block'
+        | jq '.result'
     )
 
-# Set block by known hash.
+# Set transfers by known block hash.
 _BLOCK_BY_HASH=$(
-    $CASPER_CLIENT get-block \
+    $CASPER_CLIENT get-block-transfers \
         --node-address "$_NODE_ADDRESS" \
         --block-identifier "$_BLOCK_HASH" \
-        | jq '.result.block'
+        | jq '.result'
     )
 
-# Set block by known height.
+# Set transfers by known block height.
 _BLOCK_BY_HEIGHT=$(
-    $CASPER_CLIENT get-block \
+    $CASPER_CLIENT get-block-transfers \
         --node-address "$_NODE_ADDRESS" \
         --block-identifier $_BLOCK_HEIGHT \
-        | jq '.result.block'
+        | jq '.result'
     )
 
 # Render.
