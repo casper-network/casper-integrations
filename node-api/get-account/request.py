db1fc8b097e40150a87534d8ab8578c0ddb1871d
Mark A. Greenslade <mark@casperlabs.io>
2021-05-03 22:28:53 +0200

    1. Improved pycspr code.

--- node-api/get-account/request.py
+++ node-api/get-account/request.py
@@ -16,9 +16,6 @@ pycspr.initialise(
 # A known account public key.
 _ACCOUNT_KEY = "01cb99ab80325d73552c7c0b8d10d8cb2d19116b1f233431751fe82f9c25db51c1"
 
-# A known on-chain account hash.
-_ACCOUNT_HASH = "21e8cee83ced9ff174357dc5d9b797f259994354993f01e18db49cda2a7700b3"
-
 # A known state of the linear block chain at which to query.
 _STATE_ROOT_HASH = "33e257bc70f7094d030a18f8aede3d58d8e202fb946810ce3292625fe853b636"
 
@@ -31,20 +28,13 @@ def main():
     assert _ACCOUNT_HASH == pycspr.get_account_hash(_ACCOUNT_KEY)
 
     # Set account information by account key.
-    account_info_1 = pycspr.get_account_info(_ACCOUNT_KEY, _STATE_ROOT_HASH)   
-
-    # Set account information by account hash.
-    account_info_2 = pycspr.get_account_info_by_account_hash(_ACCOUNT_HASH, _STATE_ROOT_HASH)
-
-    # Verify account information equivalence.
-    assert account_info_1 == account_info_2
+    account_info = pycspr.get_account_info(_ACCOUNT_KEY, _STATE_ROOT_HASH)   
 
     print("-----------------------------------------------------------------------------------------------------")
     print(f"QUERIED TEST-NET NODE {pycspr.CONNECTION} @ {_STATE_ROOT_HASH}")
     print("-----------------------------------------------------------------------------------------------------")
     print(f"A/C key = {_ACCOUNT_KEY}")
-    print(f"A/C hash = {_ACCOUNT_HASH}")
-    print(f"A/C on-chain info = {json.dumps(account_info_1, indent=4)}") 
+    print(f"A/C on-chain info = {json.dumps(account_info, indent=4)}") 
     print("-----------------------------------------------------------------------------------------------------")
 
 
