54a42ac532e482a0d9089ba3afd6882c1c1e80a8
Mark A. Greenslade <mark@casperlabs.io>
2021-07-21 12:41:35 +0200

    1. Extended python get-auction-info to include optional block hash.

--- node-api/get-auction-info/request.py
+++ node-api/get-auction-info/request.py
@@ -8,6 +8,12 @@ import pycspr
 # A known casper test-net node address.
 _NODE_ADDRESS = os.getenv("CASPER_NODE_ADDRESS", "3.136.227.9")
 
+# A known block hash.
+_BLOCK_HASH: bytes = bytes.fromhex("c7148e1e2e115d8fba357e04be2073d721847c982dc70d5c36b5f6d3cf66331c")
+
+# A known block height.
+_BLOCK_HEIGHT: int = 20652
+
 
 def main():
     """Retrieves on-chain auction information.
@@ -17,12 +23,21 @@ def main():
     client = pycspr.NodeClient(pycspr.NodeConnectionInfo(host=_NODE_ADDRESS))
 
     # Set auction info scoped by current era.
-    auction_info: dict = client.queries.get_auction_info()
+    auction_info_1: dict = client.queries.get_auction_info()
+
+    # Set auction info scoped by known hash.
+    auction_info_2: dict = client.queries.get_auction_info(_BLOCK_HASH)
+
+    # Set auction info scoped by known height.
+    auction_info_3: dict = client.queries.get_auction_info(_BLOCK_HEIGHT)
+
+    # Verify auction information equivalence.
+    assert auction_info_2 == auction_info_3
 
     print("-----------------------------------------------------------------------------------------------------")
     print(f"QUERIED TEST-NET NODE {_NODE_ADDRESS}")
     print("-----------------------------------------------------------------------------------------------------")
-    print(f"Auction information = {json.dumps(auction_info, indent=4)}")
+    print(f"Auction information = {json.dumps(auction_info_1, indent=4)}")
     print("-----------------------------------------------------------------------------------------------------")
 
 
